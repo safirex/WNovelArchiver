a53e47161ae7f3a99ec8f155cf3724482591aa9d
chatinEu <eudes.chatin@gmail.com>
2022-12-10 19:21:31 +0100

    fix tests for ci/cd

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -7,7 +7,7 @@ sys.path.append('..\src')
 sys.path.append('src')
 
 from main_functions import findNovel
-from Downloaders import KakuyomuNovel, Novel
+from Downloaders import *
 from archive_updater import check_env
 
 
@@ -18,27 +18,67 @@ from archive_updater import check_env
 
 class TestMainFunctions(unittest.TestCase):
 
-    def setUp(self):
-        os.mkdir('novel_list')
 
-    def test_find(self):
+    def setUp(self):
         self.assertTrue(len(findNovel("")) ==
                         len(os.listdir('novel_list')))
     
-    def builder(self):
-        novel=Novel(16816452220453312822)
-        self.assertTrue(novel.__class__==Novel.__class__)
-        novel=novel.updateObject();
-        self.assertTrue(novel.__class__==KakuyomuNovel.__class__)
-
-# def suite():
-#     suite= unittest.TestSuite()
-#     suite.addTest()
+    def test_builder(self):
+       
+        x = Novel('n18n2935bp', 'memory rewrite ')
+        x = x.updateObject()
+        self.assertTrue(x.__class__ == N18SyosetuNovel )
+                
+    '''        tests Syosetu           '''
+    def test_SyosetuNovel(self):
+        novel=Novel('n5080fi')
+        self.assertTrue(novel.__class__ == Novel)
+        novel=novel.updateObject()
+        self.assertTrue(novel.__class__ == SyosetuNovel)
+        
+    def test_fetchSyosetu(self):
+        novel=Novel('n5080fi')
+        novel=novel.updateObject()
+        html = novel.fetchTOCPage()
+        self.assertTrue(html != '')
+        self.assertRegex(html,'novel_ex')
+        
+        
+    '''   tests kakyomu           '''
+    def test_KakyomuNovel(self):
+        novel=Novel('16816452220453312822')
+        self.assertTrue(novel.__class__ == Novel)
+        novel=novel.updateObject()
+        self.assertTrue(novel.__class__ == KakuyomuNovel)
+    
+    def test_fetchKakyomu(self):
+        novel=Novel('16816452220453312822')
+        novel=novel.updateObject()
+        html = novel.fetchTOCPage()
+        self.assertTrue(html != '')
+        self.assertRegex(html,'workTitle')
+        
+        
+        
+        '''   tests n18   '''
+    def test_n18SyosetuNovel(self):
+        novel=Novel('n18n6426w')
+        self.assertTrue(novel.__class__ == Novel)
+        novel=novel.updateObject()
+        self.assertTrue(novel.__class__ == N18SyosetuNovel)
+    
+    def test_fetchN18Syosetu(self):
+        novel=Novel('n18n6426w')
+        novel=novel.updateObject()
+        html = novel.fetchTOCPage()
+        self.assertTrue(html != '')
+        self.assertRegex(html,'novel_ex')
+        
 
 
 
 if __name__ == '__main__':
-    check_env()
+    os.mkdir('novel_list')
     unittest.main()
     # runner = unittest.TextTestRunner()
     # runner.run(suite())
