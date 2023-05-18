caf5faa377a538cb9cf87debe30dac6f180bbd38
Safirex <eudes.chatin@gmail.com>
2023-05-18 21:44:52 +0200

    rework argparse for usable cli help

--- archive_updater.py
+++ archive_updater.py
@@ -74,71 +74,71 @@ def check_env():
     except FileNotFoundError: 
         os.mkdir('novel_list')
 
+def option_download(args):
+    keep_text_format=args.md
+    if args.i:
+        print(args.i)
+        download_cli(args.i)
+    else: 
+        print("downloading")
+        download(keep_text_format)
+    
 
+def option_update(args):
+    keep_text_format=args.md
+    regex=args.r
+    archiveUpdate(findNovel(regex),keep_text_format)
+       
+def option_zip(args):
+    regex=args.r
+    out=args.o
+    compressAll(regex,out)
+
+def option_test(args):
+    print("test")
+
+def option_status():
+    getFolderStatus()
+    
+    
 def parser():
-    parser = argparse.ArgumentParser(description="""        c to compress novels in zip
-        d to download input.txt list
-        s to update status.csv
-        u to update novels""",
-        formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument("mode",
-        help="put the letter of argument c/d/s/u",
-        type=str,default=argparse.SUPPRESS)
-
-    parser.add_argument("-r", help="regex of entree for compression selection (select * containing regex)",
-        type=str,default=argparse.SUPPRESS)
-    parser.add_argument("-o", help="output directory (only works for compression)",
-        type=str,default=argparse.SUPPRESS)
-    parser.add_argument("-f", help="force",action='store_true'
-        ,default=argparse.SUPPRESS)
-    parser.add_argument("-md", help="format",action='store_true'
-        ,default=argparse.SUPPRESS)   
-    parser.add_argument("-i", help="for a command line download input",
-        type=str, default=argparse.SUPPRESS)  
-
+    parser = argparse.ArgumentParser(argument_default="--help")
+    # parser.add_argument("mode",
+    #     help="put the letter of argument c/d/s/u",
+    #     type=str,
+    #     default=argparse.SUPPRESS)
+    subparsers = parser.add_subparsers()
+    
+    
+    parser_download = subparsers.add_parser('download',aliases='d', help='download novels found in input.txt')
+    parser_download.add_argument('-i', type=str, help='change data source to CLI (next arg should be a novel line code;name)')
+    parser_download.add_argument('-md', action='store_true', help='change text format to html/md')
+    parser_download.set_defaults(func=option_download)
+    
+    parser_update = subparsers.add_parser('update',aliases="u", help='update novel folders found in novel_list')
+    parser_update.add_argument('-r', type=str, help='set a regex filtering the novels', default='')
+    parser_update.add_argument('-md', action='store_true', help='change text format to html/md')
+    parser_update.set_defaults(func=option_update)
+    
+    parser_zip = subparsers.add_parser('zip', help='zip help')
+    parser_zip.add_argument('-o', type=str, help='output directory')
+    parser_zip.add_argument('-r', type=str, help='set a regex filtering the novels', default='')
+    parser_zip.set_defaults(func=option_zip)
+    
+    parser_zip = subparsers.add_parser('test',aliases="t", help='for development tests')
+    parser_zip.set_defaults(func=option_test)
+    
+    parser_zip = subparsers.add_parser('status',aliases="s", help='set status of every local novel in csv file')
+    parser_zip.set_defaults(func=option_status)
+    
+    
     args = parser.parse_args()
     print(args)
-    regex=''
-    keep_text_format=False
-
-    if args.mode:
-        if hasattr(args, 'md'):
-            keep_text_format=True
-
-        if hasattr(args, 'r'):
-            regex=args.r
-
-        if(args.mode==downloadInput):
-            if hasattr(args, 'i'):
-                print(args.i)
-                download_cli(args.i)
-            else: 
-                print("downloading")
-                download(keep_text_format)
-            
-        elif(args.mode==updateInput):
-            archiveUpdate(findNovel(regex),keep_text_format)
-            
-        elif(args.mode==statusInput):
-            getFolderStatus()
-
-        elif(args.mode==fullupdateInput):
-            if hasattr(args, 'f'):
-                archiveFullUpdate(findNovel(regex),True)
-            else:
-                archiveFullUpdate(findNovel(regex))
-        elif(args.mode=='t'):
-            dev_tests()
-            
-
-        elif(args.mode==compressInput):
-            print('compression')
-            print(args)
-            out=''
-            if hasattr(args, 'o'):
-                out=args.o
-            compressAll(regex,out)
-
+    if(hasattr(args,"func")):
+        print(args.func(args))
+    else :
+        parser.print_help()
+    
 if __name__ == '__main__':
     check_env()
     parser()
